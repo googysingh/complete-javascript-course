ba1409cd92d60d0e815585a47d6adc7479167d43
Gurpreet Singh <googysingh@gmail.com>
2024-04-28 12:50:13 +0530

    Basic and Values_Variables=28/04

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+//Basic Hello World
+let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is Fun!!');
+//41 + 30 + 70; //This will not print in the console of the browser
+console.log(41 + 30 + 70);
+
+//Values and Variables
+console.log('John');
+console.log(23);
+
+let firstName = 'Gurpreet';
+
+console.log(firstName);
+
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 'this is not possible';  Syntax Error not possible
+//Practice Assignments of Values and Variables
+
+let country = 'India';
+let continent = 'Uttar Pradesh';
+let population = 1000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
